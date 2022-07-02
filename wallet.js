995b2cc75fa4408793e55fbb800c06928d70b0c3
Nick Schauer <nschauer109@gmail.com>
2022-07-02 18:54:37 -0400

    NFC icon, bottom grey bar

--- wallet.js
+++ wallet.js
@@ -4,16 +4,19 @@ $('.container').on('click', '.button', function() {
     let clickedButton = $(this);
     let topCardHolder = $('.card-large');
     let topCard = topCardHolder.children('.button');
+    let nfc = $('.nfc');
     if(state===0){
         buttons.addClass('expanded');
         topCard.appendTo($('.button-wrap'));
         topCardHolder.remove();
         $('.container').prepend('<div class="card-large"></div>');
+        nfc.css('display', 'none');
         state = 1;
     }
     else if(state===1){
         clickedButton.appendTo(topCardHolder);
         buttons.removeClass('expanded');
+        nfc.css('display', 'block');
         state = 0;
     }
 });

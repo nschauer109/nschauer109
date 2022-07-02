7ae5547d215fbd905e2082167df95e320b391add
Nick Schauer <nschauer109@gmail.com>
2022-07-01 23:21:24 -0400

    More bug fixes

--- wallet.js
+++ wallet.js
@@ -1,16 +1,15 @@
 let state = 0;
 $('.container').on('click', '.button', function() {
     let buttons = $('.button');
+    let clickedButton = $(this);
+    let topCardHolder = $('.card-large');
+    let topCard = topCardHolder.children('.button');
     if(state===0){
         buttons.addClass('expanded');
+        topCard.appendTo($('.button-wrap'));
         state = 1;
     }
     else if(state===1){
-        let clickedButton = $(this);
-        let topCardHolder = $('.card-large');
-        let topCard = topCardHolder.children('.button');
-
-        topCard.appendTo($('.button-wrap'));
         clickedButton.appendTo(topCardHolder);
         buttons.removeClass('expanded');
         state = 0;

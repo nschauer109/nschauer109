0944a61f9fdc2e1178e04f230e6709bf44d39524
Nick Schauer <nschauer109@gmail.com>
2022-07-01 23:28:33 -0400

    Meta tag and debugs

--- wallet.js
+++ wallet.js
@@ -5,12 +5,16 @@ $('.container').on('click', '.button', function() {
     let topCardHolder = $('.card-large');
     let topCard = topCardHolder.children('.button');
     if(state===0){
+        console.log("Expanding cards");
         buttons.addClass('expanded');
+        console.log("Move top to bottom");
         topCard.appendTo($('.button-wrap'));
         state = 1;
     }
     else if(state===1){
+        console.log("Move clicked to top");
         clickedButton.appendTo(topCardHolder);
+        console.log("Collapse cards");
         buttons.removeClass('expanded');
         state = 0;
     }

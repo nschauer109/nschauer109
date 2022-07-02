f44a1c4317124f8dc6aaa56d4c0157b96f6e1b13
Nick Schauer <nschauer109@gmail.com>
2022-07-02 19:36:23 -0400

    styling changes

--- wallet.js
+++ wallet.js
@@ -5,19 +5,27 @@ $('.container').on('click', '.button', function() {
     let topCardHolder = $('.card-large');
     let topCard = topCardHolder.children('.button');
     let nfc = $('.nfc');
+    let wrap = $('.button-wrap');
+    let extras = $('.ticket-extras');
     if(state===0){
         buttons.addClass('expanded');
+        wrap.css('top', '120px');
+        wrap.css('bottom', '');
         topCard.appendTo($('.button-wrap'));
         topCardHolder.remove();
         $('.container').prepend('<div class="card-large"></div>');
+        extras.css('display', 'none');
         nfc.css('display', 'none');
         nfc.css('top', '40%');
         state = 1;
     }
     else if(state===1){
         if(clickedButton.hasClass('buckeyes')){
-            nfc.css('top', '60%');
+            nfc.css('top', '55%');
+            extras.css('display', 'block');
         }
+        wrap.css('top', '');
+        wrap.css('bottom', '-80px');
         clickedButton.appendTo(topCardHolder);
         buttons.removeClass('expanded');
         nfc.css('display', 'block');

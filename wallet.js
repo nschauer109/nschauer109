d091de0a915e2ed5f6b29a354e4f8e382e1232e5
Nick Schauer <nschauer109@gmail.com>
2022-07-01 23:33:22 -0400

    Trying to fix jquery bug??

--- wallet.js
+++ wallet.js
@@ -5,16 +5,14 @@ $('.container').on('click', '.button', function() {
     let topCardHolder = $('.card-large');
     let topCard = topCardHolder.children('.button');
     if(state===0){
-        console.log("Expanding cards");
+        buttons.css('display', 'none');
         buttons.addClass('expanded');
-        console.log("Move top to bottom");
         topCard.appendTo($('.button-wrap'));
+        buttons.css('display', 'block');
         state = 1;
     }
     else if(state===1){
-        console.log("Move clicked to top");
         clickedButton.appendTo(topCardHolder);
-        console.log("Collapse cards");
         buttons.removeClass('expanded');
         state = 0;
     }
